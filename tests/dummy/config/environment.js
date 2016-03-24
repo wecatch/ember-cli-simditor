91e87dae51beeea9ea099c24173d32a65aaed21a
zhaoyongqiang <zhyq0826@gmail.com>
2016-03-24 18:56:11 +0800

    fix dummy gh-pages enviroment

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -27,6 +27,10 @@ module.exports = function(environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
+  if(environment === 'gh-pages'){
+    ENV.baseURL = '/ember-cli-simditor'
+  }
+
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
