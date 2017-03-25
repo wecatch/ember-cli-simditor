d07f0627248ce75985b8be7a94d0a68e1696221b
zhaoyongqiang <zhyq0826@gmail.com>
2017-03-25 14:38:49 +0800

    fix tests

--- tests/integration/components/simditor-editor-test.js
+++ tests/integration/components/simditor-editor-test.js
@@ -9,6 +9,7 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
   var model = {'content': 'hello emberjs'};
+  this.set('model', model);
   this.render(hbs`{{simditor-editor value=model editor=(mut editor) }}`);
   assert.notEqual(typeof this.get('editor'), 'undefined');
   assert.notEqual(this.get('editor'), null);
